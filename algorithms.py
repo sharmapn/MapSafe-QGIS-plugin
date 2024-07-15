22ec4418734570518f6172d9537e52b06fb2602e
PankajSharma <pankajeshwara@gmail.com>
2024-07-15 20:05:24 +1200

    Add files via upload

--- algorithms.py
+++ algorithms.py
@@ -26,7 +26,7 @@ from qgis.core import (
 )
 from qgis import processing
 
-#from .utilities import singlepartGeometries
+from .singlepartGeo import singlepartGeometries
 
 
 class CreateH3GridInsidePolygonsProcessingAlgorithm(QgsProcessingAlgorithm):
